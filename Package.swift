95b581f62b84434a0f983e100d4dbd0cec9a7722
project-access-token <group_122552022_bot_c9ed9ee134fc5d1058cebb4e160abc23@noreply.gitlab.com>
2026-03-16 08:06:46 +0000

    chore: Update to v0.3.1-dev.10

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@
 
 import PackageDescription
 
-let version = "0.3.1-dev.9"
-let checksum = "3d8656f2b585d4c0915b8b24fa85d319b239bf3c0a0b903495e9c36429941a2c" // Updated by CI on release
+let version = "0.3.1-dev.10"
+let checksum = "bd0312296ec4ae28d039f4c360759a1887dcb09153e893b5f9805fdb946d69c7" // Updated by CI on release
 
 let package = Package(
     name: "VauchiPlatform",

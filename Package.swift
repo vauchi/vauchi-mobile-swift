5dbea56e78e8a456007b3babb0d76e6a8854e6e3
Vauchi CI <ci@vauchi.app>
2026-03-29 07:06:16 +0000

    chore: Update to v0.11.1

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@
 
 import PackageDescription
 
-let version = "0.11.0"
-let checksum = "9ab3cf0bad6e630b105a63c5517243d235a2d7105f40b2196eb620d97697fc23" // Updated by CI on release
+let version = "0.11.1"
+let checksum = "4b07ad4800c8881460f3a0a2f136bdfcf44d8972e80729e2b89b016854e6de60" // Updated by CI on release
 
 let package = Package(
     name: "VauchiPlatform",

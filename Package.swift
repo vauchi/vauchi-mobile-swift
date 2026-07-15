644c9bdba4844c8ff324261047a1802152c3d1c3
project-access-token <group_122552022_bot_c9ed9ee134fc5d1058cebb4e160abc23@noreply.gitlab.com>
2026-07-15 04:16:09 +0000

    chore: Update to v0.55.2

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@
 import Foundation
 import PackageDescription
 
-let version = "0.55.1"
-let checksum = "c391124b693eaaf271118b2acba027fb9bf0c935311ce0eebf9a08fe91f2fd43" // Updated by CI on release
+let version = "0.55.2"
+let checksum = "e2d90186635c62dcf4bade2e63ed95f2a0e1e172e4cc2c50def44c059d38265a" // Updated by CI on release
 
 // Binary-target source: defaults to the published URL artifact (verified
 // by `checksum` above). When VAUCHI_PLATFORM_USE_LOCAL_XCFRAMEWORK is set
